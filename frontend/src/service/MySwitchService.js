88d640d6f51741c23f684fc7c60437bd851c5cb7
paulakhalil <s.paulakhalil@icloud.com>
2024-08-15 21:22:58 +0300

    updated switch services

--- frontend/src/service/MySwitchService.js
+++ frontend/src/service/MySwitchService.js
@@ -1,37 +1,17 @@
 export const MySwitchService = {
   getMySwitchesData() {
-    return [
-      {
-        id: "1000",
-        name: "Bamboo Watch",
-        inventoryStatus: "INSTOCK",
-        IPaddress: "10.139.50.52",
-      },
-      {
-        id: "1027",
-        name: "Yellow Earbuds",
-        inventoryStatus: "INSTOCK",
-        IPaddress: "10.139.50.53",
-      },
-      {
-        id: "1028",
-        name: "Yoga Mat",
-        inventoryStatus: "INSTOCK",
-        IPaddress: "10.139.50.54",
-      },
-      {
-        id: "1029",
-        name: "Yoga Set",
-        inventoryStatus: "INSTOCK",
-        IPaddress: "10.139.50.55",
-      },
+    const switches = JSON.parse(localStorage.getItem("myswitches") || "[]");
+    return switches.length > 0 ? switches : [
     ];
   },
 
   getMySwitches() {
     return Promise.resolve(this.getMySwitchesData());
-  }
+  },
   
+  saveMySwitches(switches) {
+    localStorage.setItem("myswitches", JSON.stringify(switches));
+  }
 };
 
 export default MySwitchService;
