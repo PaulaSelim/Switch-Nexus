527a52e91c3c6337779761ce1e20689e3b7ee327
paulakhalil <s.paulakhalil@icloud.com>
2024-08-17 21:05:10 +0300

    refactored code and updated SSH dependencies to accommodate SwitchDetails functionality

--- internal/interfaces/wails_interfaces/wails_interfaces.go
+++ internal/interfaces/wails_interfaces/wails_interfaces.go
@@ -1,18 +1,19 @@
 package wails_interfaces
 
 import (
+	"Switch-Nexus/internal/utilities/ssh"
 	"context"
-
-	"Switch-Nexus/internal/services/switch_services"
+	"log"
 )
 
 type WailsInterface struct {
 	ctx     context.Context
-	service *switch_services.SwitchService
+	user	string
+	pass    string
 }
 
-func New(service *switch_services.SwitchService) *WailsInterface {
-	return &WailsInterface{service: service}
+func New(user, password string) *WailsInterface {
+	return &WailsInterface{user: user, pass: password}
 }
 
 func (a *WailsInterface) Startup(ctx context.Context) {
@@ -23,18 +24,40 @@ func (a *WailsInterface) Shutdown(ctx context.Context) {
 	a.ctx = ctx
 }
 
-func (a *WailsInterface) GetStatus() (string, error) {
-	return a.service.GetStatus()
+func (a *WailsInterface) GetStatus(host string) (string, error) {
+	log.Printf("GetStatus on: %s:",host)
+	return "Status", nil
 }
 
-func (a *WailsInterface) GetVLAN() (string, error) {
-	return a.service.GetVLAN()
+func (a *WailsInterface) GetVLAN(host string) (string, error) {
+	log.Printf("GetVLAN on: %s, with credentials: user(%s) pass(%s)", host, a.user, a.pass)
+	sshConnector, err := ssh.NewConnector(host, a.user, a.pass)
+	if err != nil {
+		log.Fatalf("Failed to connect to SSH: %v", err)
+	}
+	// command := "ipconfig"
+	command := "show vlan"
+	return sshConnector.RunCommand(command)
 }
 
-func (a *WailsInterface) GetCDPNeighbors() (string, error) {
-	return a.service.GetCDPNeighbors()
+func (a *WailsInterface) GetCDPNeighbors(host string) (string, error) {
+	log.Printf("GetCDPNeighbors on: %s, with credentials: user(%s) pass(%s)", host, a.user, a.pass)
+	sshConnector, err := ssh.NewConnector(host, a.user, a.pass)
+	if err != nil {
+		log.Fatalf("Failed to connect to SSH: %v", err)
+	}
+	// command := "ipconfig"
+	command := "show cdp neighbors"
+	return sshConnector.RunCommand(command)
 }
 
-func (a *WailsInterface) GetIPInterface() (string, error) {
-	return a.service.GetIPInterface()
+func (a *WailsInterface) GetIPInterface(host string) (string, error) {
+	log.Printf("GetIPInterface on: %s, with credentials: user(%s) pass(%s)", host, a.user, a.pass)
+	sshConnector, err := ssh.NewConnector(host, a.user, a.pass)
+	if err != nil {
+		log.Fatalf("Failed to connect to SSH: %v", err)
+	}
+	// command := "systeminfo"
+	command := "show ip interface brief"
+	return sshConnector.RunCommand(command)
 }
