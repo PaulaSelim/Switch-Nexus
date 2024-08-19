d4b846991a3f3934bd3947eeb19ef683cab7a9c6
paulakhalil <s.paulakhalil@icloud.com>
2024-08-20 00:10:06 +0300

    refactored code and updated router

--- frontend/src/views/SwitchDetails.vue
+++ frontend/src/views/SwitchDetails.vue
@@ -3,26 +3,21 @@ import CDPNeighborsDetails from "../components/CDPNeighborsDetails.vue";
 import IPInterfaceDetails from "../components/IPInterfaceDetails.vue";
 import VLANDetials from "../components/VLANDetials.vue";
 import { useRoute } from "vue-router";
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 import { useRouter } from "vue-router";
 import MegaMenu from "primevue/megamenu";
 
 const route = useRoute();
 const router = useRouter();
 
-function navigateToPage() {
-  router.push("/");
-}
-
 var address = ref(route.params.address.replace(/\-/g, ".").replace(/\=/g, ":"));
-console.log(address.value);
 </script>
 
 <template>
   <div>
     <MegaMenu :model="menubar" class="menubar">
       <template #start>
-        <RouterLink to="/">
+        <RouterLink to="/home">
           <p class="backButton">
             <i
               class="pi pi-angle-left"
